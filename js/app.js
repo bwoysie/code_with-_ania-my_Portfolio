32be891fd33f0e59ec60c11d9806bbb94788c483
Patrick Harrison <bwoysie876@gmail.com>
2024-03-03 12:25:04 -0500

    adding files

---
+++ js/app.js
@@ -0,0 +1,7 @@
+//our javascript goes here
+const hamburgerButton = document.querySelector('.hamburger')
+const hamburgerList = document.querySelector('.hamburger-list-container')
+
+hamburgerButton.addEventListener('click', () => {
+  hamburgerList.classList.toggle('hidden')
+})
